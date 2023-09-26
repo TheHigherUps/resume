8145cf811767285538a4d24f99f432caf6cd79bf
TheHigherUps <classified@thehigherups.org>
2023-09-26 10:47:32 -0400

    started to add projecst list to data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -39,3 +39,37 @@ export const mainSkills = [
         additionalText: "(Just Learning)",
     },
 ]
+
+export const projects = [
+    {
+        title: "TheHigherUps",
+        subtitle: "2019 - current",
+        description: `Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Fugiat quas ipsam cum placeat, animi quod
+        quasi libero tempore accusantium id in, error esse
+        unde quos doloribus provident veniam repudiandae
+        magnam ratione, sint fuga eos. Pariatur laboriosam
+        mollitia velit, earum molestias, consectetur impedit
+        deleniti quam cumque quasi quidem! Veniam voluptates
+        ullam voluptatum maxime labore. Nesciunt nemo earum
+        doloremque eligendi ratione repellendus sed, commodi
+        praesentium! Doloremque, consequuntur. Consequuntur
+        molestias rerum exercitationem blanditiis commodi
+        optio itaque dicta hic ex labore quaerat molestiae
+        dolorem et ullam ad, quisquam necessitatibus odio
+        officiis. Distinctio laborum aperiam expedita quas
+        animi ratione! Neque illum explicabo quas libero
+        exercitationem!`,
+        tags: ["TheHigherUps", "Shop", "Beans"],
+        links: [
+            {
+                title: "Github",
+                href: "#",
+            },
+            {
+                title: "Preview",
+                href: "#",
+            },
+        ],
+    },
+]
