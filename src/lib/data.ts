2372c9a5e0534a4a3ef3bb4a2d4ad215729ce47c
TheHigherUps <classified@thehigherups.org>
2023-09-26 12:25:45 -0400

    completed projects data const

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -72,4 +72,54 @@ export const projects = [
             },
         ],
     },
+    {
+        title: "Firebase Admin Panel",
+        subtitle: "2022",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Vero iure pariatur eaque veniam doloremque
+        quam, natus nihil laboriosam asperiores. Ea ab
+        laboriosam sit quia est sequi delectus dolorum
+        deserunt praesentium unde fugit impedit commodi
+        consequuntur magnam numquam molestias ad, modi
+        optio! Omnis, similique dicta totam, nesciunt
+        perspiciatis nulla quam esse nostrum minus a tempore
+        rerum dolore enim. Porro veritatis ipsum labore
+        consequatur, dicta est fuga numquam maxime corporis
+        totam ipsa nesciunt omnis sapiente tempora voluptas!`,
+        tags: ["Firebase", "MaterialUI", "React"],
+        links: [
+            {
+                title: "Github",
+                href: "#",
+            },
+            {
+                title: "Preview",
+                href: "#",
+            },
+        ],
+    },
+    {
+        title: "Calendar",
+        subtitle: "2023",
+        description: `Lorem ipsum dolor sit amet, consectetur adipisicing
+        elit. Adipisci repellendus eligendi laboriosam
+        voluptatum inventore harum officiis aliquid debitis
+        deleniti laudantium, suscipit voluptate dolorem
+        iure! Odit illo perferendis velit animi dignissimos
+        officia quas aliquid quia doloribus asperiores,
+        omnis adipisci aliquam aperiam, quam praesentium
+        cumque commodi voluptas magnam ad maxime assumenda
+        sit.`,
+        tags: ["Supabase", "React", "NextJS"],
+        links: [
+            {
+                title: "Github",
+                href: "#",
+            },
+            {
+                title: "Preview",
+                href: "#",
+            },
+        ],
+    },
 ]
